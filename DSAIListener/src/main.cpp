c9519dd880c5efc40a7049a1aa2fc5a9d32ca6b0
Simo Angelov <simeon_g_angelov@abv.bg>
2019-06-08 16:38:18 +0300

    DSAI: Added BasicJsonParser

--- DSAIListener/src/main.cpp
+++ DSAIListener/src/main.cpp
@@ -1,15 +1,29 @@
 #include <iostream>
 #include "DSAIListener.h"
+#include "utils/BasicJsonParser.h"
 using namespace std;
 
 int main()
 {
     cout << "Hello World!" << endl;
-    DSAIListener test("192.168.1.6 ", 54000, [](DSAIListener* listener, int nSocketID, const std::string& strMsg)
+    BasicJsonParser parser;
+    SDSAIData sData;
+    sData.nSpeed = 1200;
+    sData.nRPM = 2333;
+    sData.fEngineTemp = 13.6f;
+    sData.bFogLamp = true;
+    sData.fGPSLatitude = 75.1244f;
+    sData.fAirTemp = 41.02f;
+    sData.bHandbrake = true;
+    sData.nFuel = 1700;
+    parser.Stringify(sData);
+
+
+    DSAIListener test("172.16.1.4", 54000, [&](DSAIListener* listener, int nSocketID, const std::string& strMsg)
     {
-        std::cout << "Echoing message " << strMsg
+        std::cout << "Echoing message " << parser.GetJson()
                   << " back to Client #" << nSocketID << "\n";
-        listener->Send(nSocketID, strMsg);
+        listener->Send(nSocketID, parser.GetJson());
     });
 
     if(test.Init())
